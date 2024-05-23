73d2c0e7f3b05aadee05e7d0539a56a7affd79f5
eliao13 <101660088+eliao13@users.noreply.github.com>
2024-05-23 12:30:29 -0700

    install vite

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+      We are launching soon! Subscribe and get notified Notify Me &copy;
+      Copyright Ping. All rights reserved.
+      <footer>
+        <p class="attribution">
+          Challenge by{" "}
+          <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+            Frontend Mentor
+          </a>
+          . Coded by <a href="#">Your Name Here</a>.
+        </p>
+      </footer>
+    </>
+  );
+}
+
+export default App;
