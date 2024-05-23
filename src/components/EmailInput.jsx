4b0c1192779cb5693d103506c020a99cf0f4d962
eliao13 <101660088+eliao13@users.noreply.github.com>
2024-05-23 12:30:47 -0700

    create basic components and import assets

---
+++ src/components/EmailInput.jsx
@@ -0,0 +1,3 @@
+export default function EmailInput() {
+  return <div>EmailInput</div>;
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,10 @@
+import EmailInput from "./EmailInput";
+
+export default function Main() {
+  return (
+    <main>
+      Main
+      <EmailInput />
+    </main>
+  );
+}
